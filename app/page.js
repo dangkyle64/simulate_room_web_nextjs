8115563dd770130775b12e0824827406354e5509
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-20 13:48:13 -0800

    initial commit
    author: dangkyle64
    date: 1/20/2025

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Next.js App!</h1>
+        <p>This is a basic Next.js application built from scratch using the new `app` directory.</p>
+      </div>
+    );
+  }
