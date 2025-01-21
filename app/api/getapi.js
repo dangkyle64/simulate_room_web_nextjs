267f1c3bb6081139ff0f52fc79ac9eaa2e9deb99
dangkyle64 <kyledang64@gmail.com>
2025-01-21 09:45:23 -0800

    create test connection between frontend and backend server
    author: dangkyle64
    date: 1/21/2025

---
+++ app/api/getapi.js
@@ -0,0 +1,18 @@
+const fetchData = async () => {
+    try {
+        const response = await fetch('http://localhost:5000/api/hello');
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        };
+
+        const data = await response.json();
+        console.log(data.message);
+        return data;
+
+    } catch(error) {
+        console.log('There was an error getting the data: ', error);
+    };
+};
+
+fetchData();
