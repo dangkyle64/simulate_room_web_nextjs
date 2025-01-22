cb253954d71edc20a658d27f3e118bd20016c052
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-22 06:12:31 -0800

    fix furniture page to display the proper values for the json
    author: dangkyle64
    date: 1/22/2025

--- app/furniture/page.js
+++ app/furniture/page.js
@@ -2,13 +2,14 @@ const { fetchFurnitureData } = require('../api/furnitureApi')
 
 export default async function furnitureHome() {
 
-  const data = await fetchFurnitureData();
+    const data = await fetchFurnitureData();
+    const convertedData = JSON.stringify(data);
     return (
       <div>
         <h1>Skeleton Furniture Home Page</h1>
         <p>Skeleton Furniture Home Page for Simulate Room</p>
-        <p>{data}</p>
+        <p>{convertedData}</p>
         
       </div>
     );
-  }
+};
