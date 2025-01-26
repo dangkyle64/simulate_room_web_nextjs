233d42861bc917a337d8d9f42c84873f6714a6df
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-26 08:56:49 -0800

    set up basic render environment using babylon, basic cube with drag and drop interactions created for testing
    author: dangkyle64
    date: 1/26/2025

---
+++ app/render/page.js
@@ -0,0 +1,71 @@
+'use client';
+
+const BABYLON = require('@babylonjs/core');
+const { useEffect } = require('react');
+
+export default function RenderPage() {
+    
+    useEffect(() => {
+
+        const canvas = document.getElementById('renderCanvas');
+        if (!canvas) {
+            return;
+        };
+
+        const engine = new BABYLON.Engine(canvas, true, {
+            preserveDrawingBuffer: true,
+            stencil: true,
+            antialias: false,
+        });
+
+        const scene = new BABYLON.Scene(engine);
+
+        const camera = new BABYLON.ArcRotateCamera(
+            'camera1',
+            Math.PI / 2,
+            Math.PI / 2,
+            5,
+            BABYLON.Vector3.Zero(),
+            scene
+        );
+        camera.attachControl(canvas, true);
+
+        const light = new BABYLON.HemisphericLight('light1', BABYLON.Vector3.Up(), scene);
+
+        const cube = BABYLON.MeshBuilder.CreateBox('box', { size: 2 }, scene);
+        cube.position.y = 1;
+        
+        const dragBehavior = new BABYLON.PointerDragBehavior();
+        cube.addBehavior(dragBehavior);
+
+        dragBehavior.onDragStartObservable.add(() => {
+            cube.material = new BABYLON.StandardMaterial("dragMaterial", scene);
+            cube.material.diffuseColor = BABYLON.Color3.Green();
+        });
+
+        dragBehavior.onDragEndObservable.add(() => {
+            cube.material = new BABYLON.StandardMaterial("defaultMaterial", scene);
+            cube.material.diffuseColor  = BABYLON.Color3.White();
+        });
+
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+
+        window.addEventListener('resize', () => {
+            engine.resize();
+        });
+
+        return () => {
+            engine.dispose();
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>Babylon.js Cube Test</h1>    
+            {/* Rendering 3D Scene Here */}
+            <canvas id="renderCanvas" style={{ width: '100%', height: '100%'}} />
+        </div>
+    );
+};
