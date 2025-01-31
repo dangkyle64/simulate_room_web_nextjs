0ad8d363e72c042dea304403a6e449e346ad9d9d
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-31 14:24:05 -0800

    refactor out the objectInfoWindow functionality for better readability
    author: dangkyle64
    date: 1/31/2025

---
+++ app/render/_utils/objectInfoWindow.js
@@ -0,0 +1,66 @@
+const GUI = require('@babylonjs/gui');
+
+let currentPopUp = null;
+
+const objectInfoWindow = (scene, object) => {
+
+    if(currentPopUp) {
+        currentPopUp.isVisible = false;
+    };
+
+    var advancedTextureObjectInfoWindow = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+
+    var popUpObjectInfoWindow = new GUI.Rectangle();
+
+    const updatePopupWidth = () => {
+        const dynamicWidth = (window.innerWidth * 0.2) + "px"; // 20% of the window width
+        popUpObjectInfoWindow.width = dynamicWidth;
+    };
+
+    updatePopupWidth();
+
+    popUpObjectInfoWindow.height = "100vh";
+    popUpObjectInfoWindow.right = "0%";
+    popUpObjectInfoWindow.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+
+    popUpObjectInfoWindow.isHitTestVisible = true;
+    popUpObjectInfoWindow.background = "rgba(0, 0, 0, 0.7)";
+    advancedTextureObjectInfoWindow.addControl(popUpObjectInfoWindow);
+
+    var objectInfo = new GUI.TextBlock();
+    objectInfo.text = "";
+    objectInfo.color = "white";
+    popUpObjectInfoWindow.addControl(objectInfo);
+
+    let object_pos_x = object.position.x.toFixed(2);
+    let object_pos_y = object.position.y.toFixed(2);
+    let object_pos_z = object.position.z.toFixed(2);
+    objectInfo.text += "\nObject Name: " + object.name.toString();
+    objectInfo.text += "\nPosition X: " + object_pos_x.toString();
+    objectInfo.text += "\nPosition Y: " + object_pos_y.toString();
+    objectInfo.text += "\nPosition Z: " + object_pos_z.toString();
+
+    window.addEventListener("resize", function(){
+        
+        updatePopupWidth();
+        //adjustTextSize();
+    });
+
+    currentPopUp = popUpObjectInfoWindow;
+};
+
+module.exports = { objectInfoWindow };
+
+/*
+   function adjustTextSize(objectInfo) {
+
+        var width = popUpObjectInfoWindow.widthInPixels;
+        var height = popUpObjectInfoWindow.heightInPixels;
+
+        var fontSize = Math.min(width, height) / 10;
+
+        console.log("Adjusting text size to:", fontSize); 
+        objectInfo.fontSize = fontSize;
+    };
+
+*/
--- app/render/page.js
+++ app/render/page.js
@@ -6,6 +6,8 @@ const { OBJFileLoader } = require('@babylonjs/loaders');
 const GUI = require('@babylonjs/gui');
 const { useEffect, useRef } = require('react');
 
+const { objectInfoWindow } = require('./_utils/objectInfoWindow');
+
 export default function RenderPage() {
     
     const canvasRef = useRef(null);
@@ -402,63 +404,3 @@ const popUpScreen = (scene) => {
 
     popUpWindow.addControl(submitButton);
 };
-
-let currentPopUp = null;
-const objectInfoWindow = (scene, object) => {
-
-    if(currentPopUp) {
-        currentPopUp.isVisible = false;
-    };
-
-    var advancedTextureObjectInfoWindow = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
-
-    var popUpObjectInfoWindow = new GUI.Rectangle();
-
-    const updatePopupWidth = () => {
-        const dynamicWidth = (window.innerWidth * 0.2) + "px"; // 20% of the window width
-        popUpObjectInfoWindow.width = dynamicWidth;
-    };
-
-    updatePopupWidth();
-
-    popUpObjectInfoWindow.height = "100vh";
-    popUpObjectInfoWindow.right = "0%";
-    popUpObjectInfoWindow.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-
-    popUpObjectInfoWindow.isHitTestVisible = true;
-    popUpObjectInfoWindow.background = "rgba(0, 0, 0, 0.7)";
-    advancedTextureObjectInfoWindow.addControl(popUpObjectInfoWindow);
-
-    var objectInfo = new GUI.TextBlock();
-    objectInfo.text = "";
-    objectInfo.color = "white";
-    popUpObjectInfoWindow.addControl(objectInfo);
-
-    function adjustTextSize() {
-
-        var width = popUpObjectInfoWindow.widthInPixels;
-        var height = popUpObjectInfoWindow.heightInPixels;
-
-        var fontSize = Math.min(width, height) / 10;
-
-        console.log("Adjusting text size to:", fontSize); 
-        objectInfo.fontSize = fontSize;
-    };
-
-    let object_pos_x = object.position.x.toFixed(2);
-    let object_pos_y = object.position.y.toFixed(2);
-    let object_pos_z = object.position.z.toFixed(2);
-    objectInfo.text += "\nObject Name: " + object.name.toString();
-    objectInfo.text += "\nPosition X: " + object_pos_x.toString();
-    objectInfo.text += "\nPosition Y: " + object_pos_y.toString();
-    objectInfo.text += "\nPosition Z: " + object_pos_z.toString();
-
-    window.addEventListener("resize", function(){
-        
-        updatePopupWidth();
-        adjustTextSize();
-    });
-
-    currentPopUp = popUpObjectInfoWindow;
-};
-
