b9331dcd3bf1ee718284c3af1af2c9486d061f85
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-03 10:03:06 -0800

    basic webpage skeleton for testing
    author: dangkyle64
    date: 2/3/2025

---
+++ app/capture/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+const React = require('react');
+const InteractCamera = require('./_utils/interactCamera');
+
+export default function Capture() {
+    return (
+        <div>
+            <h1>Capture Furniture Image Testing</h1>
+            <InteractCamera />
+        </div>
+    );
+};
