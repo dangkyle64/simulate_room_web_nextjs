46c3e5b1fd50e4748e7934f68a37e5c011aaf0e2
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-12 12:41:52 -0800

    initial UI setup for furniture information
    author: dangkyle64
    date: 2/12/2025

--- app/furniture/page.js
+++ app/furniture/page.js
@@ -4,11 +4,23 @@ export default async function furnitureHome() {
 
     const data = await fetchFurnitureData();
     const convertedData = JSON.stringify(data);
+    //<pre>{convertedData}</pre>
     return (
       <div>
         <h1>Skeleton Furniture Home Page</h1>
         <p>Skeleton Furniture Home Page for Simulate Room</p>
-        <pre>{convertedData}</pre>
+        
+        <div className="furniture-grid">
+          {data.map(furniture => (
+            <div key={furniture.id} className="furniture-card">
+              <h2>{furniture.type}</h2>
+              <p>Length: {furniture.length}, Width: {furniture.width}, Height: {furniture.height}</p>
+              <p>Location: x:{furniture.x_position}, y:{furniture.y_position}, z:{furniture.z_position}</p>
+              <p>Rotation: x: {furniture.rotation_x} y: {furniture.rotation_y} z: {furniture.rotation_z}</p>
+            </div>
+          ))}
+        </div>
+        
         
       </div>
     );
