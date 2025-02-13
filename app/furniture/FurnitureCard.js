b87b415a8e63fed1958a4ac222e7fec6eca62b9d
dangkyle64 <kyledang64@gmail.com>
2025-02-13 09:29:30 -0800

    adjust the card to only show the title originally
    author: dangkyle64
    date: 2/13/2025

--- app/furniture/FurnitureCard.js
+++ app/furniture/FurnitureCard.js
@@ -1,13 +1,18 @@
 const React = require('react');
 const styles = require('./FurnitureCard.module.css')
 
+/*
+
+            <p>Length: {furniture.length}, Width: {furniture.width}, Height: {furniture.height}</p>
+            <p>Location: x:{furniture.x_position}, y:{furniture.y_position}, z:{furniture.z_position}</p>
+            <p>Rotation: x: {furniture.rotation_x} y: {furniture.rotation_y} z: {furniture.rotation_z}</p>
+        
+
+*/
 export default function FurnitureCard({ furniture, onClick }) {
     return (
         <div className={styles.card} onClick={() => onClick(furniture)}>
             <h2>{furniture.type}</h2>
-            <p>Length: {furniture.length}, Width: {furniture.width}, Height: {furniture.height}</p>
-            <p>Location: x:{furniture.x_position}, y:{furniture.y_position}, z:{furniture.z_position}</p>
-            <p>Rotation: x: {furniture.rotation_x} y: {furniture.rotation_y} z: {furniture.rotation_z}</p>
-        </div>
+            </div>
     );
 }
