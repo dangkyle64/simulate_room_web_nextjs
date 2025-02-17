ff6a9942a3736e2a183a90e94254ea7d53c2d52c
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-17 12:57:34 -0800

    implement endpoint for specific furniture for update endpoint usage and potential future usage in the api
    author: dangkyle64
    date: 12/17/2025

--- app/_furnitureApi/furnitureApi.js
+++ app/_furnitureApi/furnitureApi.js
@@ -17,6 +17,24 @@ async function fetchFurnitureData() {
     };
 };
 
+async function fetchSpecificFurnitureData(id) {
+    try {
+        //const response = await fetch('https://simulate-room-nodejs.onrender.com/api/furniture/');
+        const response = await fetch(`http://localhost:5000/api/furniture/${id}`);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data, response was not okay.');
+        };
+
+        const data = await response.json();
+        //console.log(data);
+        return data;
+
+    } catch(error) {
+        console.log('There was an error getting the data: ', error);
+        return;
+    };
+}
 async function createFurnitureData(newFurniture) {
     try {
         //const response = await fetch('https://simulate-room-nodejs.onrender.com/api/furniture/', {method: 'POST'});
@@ -43,6 +61,10 @@ async function createFurnitureData(newFurniture) {
 
 async function updateFurnitureData(id, updateFurniture) {
     try {
+        
+        //const url = `http://localhost:5000/api/furniture/${id}`;
+        //console.log('Sending PUT request to URL:', url);  // Double-check the URL
+
         //const response = await fetch(`https://simulate-room-nodejs.onrender.com/api/furniture/${id}`, {method: 'PUT'});
         const response = await fetch(`http://localhost:5000/api/furniture/${id}`, {
             method: 'PUT',
@@ -52,6 +74,7 @@ async function updateFurnitureData(id, updateFurniture) {
             body: JSON.stringify(updateFurniture),
         });
 
+        //console.log("----------------------------------HERE-------------------------------",response)
         if (!response.ok) {
             throw new Error('Failed to update data, response was not okay.');
         };
@@ -84,6 +107,7 @@ async function deleteFurnitureData(id) {
 
 module.exports = { 
     fetchFurnitureData,
+    fetchSpecificFurnitureData,
     createFurnitureData,
     updateFurnitureData,
     deleteFurnitureData
