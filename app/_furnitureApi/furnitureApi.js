0000179e1e2d68fd5135c1d34b0e604b28a790d0
dangkyle64 <kyledang64@gmail.com>
2025-02-20 17:37:01 -0800

    add in documentation for each of the functions to make it easier to understand the parameters that go into it without having looking through files
    author: dangkyle64
    date: 2/20/2025

--- app/_furnitureApi/furnitureApi.js
+++ app/_furnitureApi/furnitureApi.js
@@ -1,3 +1,14 @@
+/**
+ * Fetches all the furniture data from the API.
+ * 
+ * This function sends a GET request to the API to retrieve all furniture records.
+ * It will return the data if the request is successful, or an empty array if there is an error.
+ * 
+ * @async
+ * @function fetchFurnitureData
+ * @returns {Promise<Object[]>} A promise that resolves to an array of furniture objects, or an empty array if there was an error.
+ */
+
 async function fetchFurnitureData() {
     try {
         //const response = await fetch('https://simulate-room-nodejs.onrender.com/api/furniture/');
@@ -17,6 +28,17 @@ async function fetchFurnitureData() {
     };
 };
 
+/**
+ * Fetches a specific piece of furniture data by its ID.
+ * 
+ * This function sends a GET request to the API to retrieve a specific furniture record by its ID.
+ * It will return the data if the request is successful, or `undefined` if there is an error.
+ * 
+ * @async
+ * @function fetchSpecificFurnitureData
+ * @param {string} id - The unique identifier of the furniture item.
+ * @returns {Promise<Object|undefined>} A promise that resolves to the furniture object if successful, or `undefined` if there was an error.
+ */
 async function fetchSpecificFurnitureData(id) {
     try {
         //const response = await fetch('https://simulate-room-nodejs.onrender.com/api/furniture/');
@@ -34,7 +56,30 @@ async function fetchSpecificFurnitureData(id) {
         console.log('There was an error getting the data: ', error);
         return;
     };
-}
+};
+
+/**
+ * Creates a new piece of furniture data in the system.
+ * 
+ * This function sends a POST request to the API to create a new furniture record.
+ * It will return the newly created furniture data if the request is successful, or `undefined` if there is an error.
+ * 
+ * @async
+ * @function createFurnitureData
+ * @param {Object} newFurniture - The furniture data to be created.
+ * @param {string} newFurniture.type - The type of the furniture (e.g., chair, table).
+ * @param {string} newFurniture.modelUrl - The URL for the 3D model of the furniture.
+ * @param {number} newFurniture.length - The length of the furniture.
+ * @param {number} newFurniture.width - The width of the furniture.
+ * @param {number} newFurniture.height - The height of the furniture.
+ * @param {number} newFurniture.x_position - The X position of the furniture in the room.
+ * @param {number} newFurniture.y_position - The Y position of the furniture in the room.
+ * @param {number} newFurniture.z_position - The Z position of the furniture in the room.
+ * @param {number} newFurniture.rotation_x - The rotation around the X-axis.
+ * @param {number} newFurniture.rotation_y - The rotation around the Y-axis.
+ * @param {number} newFurniture.rotation_z - The rotation around the Z-axis.
+ * @returns {Promise<Object|undefined>} A promise that resolves to the created furniture object, or `undefined` if there was an error.
+ */
 async function createFurnitureData(newFurniture) {
     try {
         //const response = await fetch('https://simulate-room-nodejs.onrender.com/api/furniture/', {method: 'POST'});
@@ -59,6 +104,29 @@ async function createFurnitureData(newFurniture) {
     };
 };
 
+/**
+ * Updates an existing piece of furniture data in the system.
+ * 
+ * This function sends a PUT request to the API to update an existing furniture record by its ID.
+ * It will return the updated furniture data if the request is successful, or `undefined` if there is an error.
+ * 
+ * @async
+ * @function updateFurnitureData
+ * @param {string} id - The unique identifier of the furniture to be updated.
+ * @param {Object} updateFurniture - The updated furniture data.
+ * @param {string} updateFurniture.type - The type of the furniture.
+ * @param {string} updateFurniture.modelUrl - The URL for the 3D model of the furniture.
+ * @param {number} updateFurniture.length - The length of the furniture.
+ * @param {number} updateFurniture.width - The width of the furniture.
+ * @param {number} updateFurniture.height - The height of the furniture.
+ * @param {number} updateFurniture.x_position - The X position of the furniture in the room.
+ * @param {number} updateFurniture.y_position - The Y position of the furniture in the room.
+ * @param {number} updateFurniture.z_position - The Z position of the furniture in the room.
+ * @param {number} updateFurniture.rotation_x - The rotation around the X-axis.
+ * @param {number} updateFurniture.rotation_y - The rotation around the Y-axis.
+ * @param {number} updateFurniture.rotation_z - The rotation around the Z-axis.
+ * @returns {Promise<Object|undefined>} A promise that resolves to the updated furniture object, or `undefined` if there was an error.
+ */
 async function updateFurnitureData(id, updateFurniture) {
     try {
         
@@ -88,6 +156,17 @@ async function updateFurnitureData(id, updateFurniture) {
     };
 };
 
+/**
+ * Deletes a specific piece of furniture data from the system.
+ * 
+ * This function sends a DELETE request to the API to delete a furniture record by its ID.
+ * If the request is successful, no data is returned.
+ * 
+ * @async
+ * @function deleteFurnitureData
+ * @param {string} id - The unique identifier of the furniture to be deleted.
+ * @returns {Promise<void>} A promise that resolves to `undefined` when the data is deleted.
+ */
 async function deleteFurnitureData(id) {
     try {
         //const response = await fetch(`https://simulate-room-nodejs.onrender.com/api/furniture/${id}`, {method: 'DELETE'});
