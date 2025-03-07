3b1f511412cb5df6f0a0e4bc2946653c86bf0113
dangkyle64 <kyledang64@gmail.com>
2025-03-07 12:04:03 -0800

    extra files causing confusion so condensing into one file, will refactor as file gets bigger
    author: dangkyle64
    date: 3/7/2025

--- app/render/_components/FurnitureMenu/FurnitureMenu.js
+++ app/render/_components/FurnitureMenu/FurnitureMenu.js
@@ -1,6 +1,7 @@
-import { InteractionMenu } from './InteractionMenu';
-import { FurnitureMenuButton } from'./FurnitureMenuButton';
+import * as GUI from '@babylonjs/gui';
 import { fetchFurnitureData } from '../../../_furnitureApi/furnitureApi';
+import { ListMenu } from './ListMenu';
+
 export const FurnitureMenu = async (scene) => {
     
     let furnitureData = loadLocalFurnitureData();
@@ -24,6 +25,48 @@ export const FurnitureMenu = async (scene) => {
     FurnitureMenuButton(scene, toggleInteractionMenu);
 };
 
+const InteractionMenu = (scene, objectsArray) => {
+    var advancedTextureInteractionMenu = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UIInteractionMenu", true, scene);
+    
+    var popUpObjectInteractionMenu = new GUI.Rectangle();
+    popUpObjectInteractionMenu.width = "400px";
+    popUpObjectInteractionMenu.height = "900px";
+    popUpObjectInteractionMenu.background = "rgba(0,0,0,0.7)";
+    
+    popUpObjectInteractionMenu.isVisible = false;
+
+    popUpObjectInteractionMenu.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    popUpObjectInteractionMenu.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    
+    advancedTextureInteractionMenu.addControl(popUpObjectInteractionMenu);
+
+    ListMenu(scene, objectsArray, popUpObjectInteractionMenu);
+    popUpObjectInteractionMenu.isPointerBlocker = true;
+    return popUpObjectInteractionMenu;
+};
+
+const FurnitureMenuButton = (scene, toggleMenuCallback) => {
+    var button = GUI.Button.CreateImageOnlyButton("cornerButton", "textures/menu_icon2.png");
+    
+    button.width = "50px"; 
+    button.height = "50px";
+    button.color = "transparent";
+    button.background = "transparent"
+
+    button.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+
+    button.left = "-10px";
+    button.top = "10px";
+    
+    var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+    advancedTexture.addControl(button);
+
+    button.onPointerUpObservable.add(() => {
+        toggleMenuCallback();
+    });
+};
+
 const saveLocalFurnitureData = (data) => {
     localStorage.setItem('furnitureData', JSON.stringify(data));
 };
@@ -31,4 +74,4 @@ const saveLocalFurnitureData = (data) => {
 const loadLocalFurnitureData = () => {
     const data = localStorage.getItem('furnitureData');
     return data ? JSON.parse(data) : null;
-}
+};
--- app/render/_components/FurnitureMenu/ListMenu.js
+++ app/render/_components/FurnitureMenu/ListMenu.js
@@ -1,5 +1,4 @@
 import * as GUI from '@babylonjs/gui';
-import { ListItemBase } from './ListItem';
 
 export const ListMenu = (scene, objectArray, popUpObjectInteractionMenu) => {
     var stackPanel = new GUI.StackPanel();
@@ -12,4 +11,69 @@ export const ListMenu = (scene, objectArray, popUpObjectInteractionMenu) => {
         ListItemBase(scene, obj, stackPanel);
     });
 };
+
+const ListItemBase = (scene, obj, parentPanel) => {
+    var listItemStack = new GUI.TextBlock();
+    listItemStack.width = "100%";
+    listItemStack.height = "10px";
+
+    var header = listItem(obj, parentPanel);
+    var listDetails = listItemDetails(obj, parentPanel);
+    
+    listDetails.isVisible = false;
+
+    header.onPointerClickObservable.add(() => {
+        if (!listDetails.isVisible) {
+            listDetails.isVisible = true;
+            console.log('Change visible to true');
+        } else {
+            listDetails.isVisible = false;
+            console.log('Change visible to false');
+        };
+    });
+
+    parentPanel.addControl(listItemStack);
+};
+
+const listItem = (obj, listItemStack) => {
+    var listItemHeader = new GUI.TextBlock();
+    listItemHeader.width = "100%";
+    listItemHeader.height = "40px";
+    listItemHeader.marginBottom = "10px";
+
+    listItemHeader.text = `Type ${obj.type}`
+    listItemHeader.color = "white";
+
+    listItemStack.addControl(listItemHeader);
+
+    listItemHeader.onPointerEnterObservable.add(() => {
+        listItemHeader.color = "yellow";
+        listItemHeader.fontSize = 20;
+    });
+
+    listItemHeader.onPointerOutObservable.add(() => {
+        listItemHeader.color = "white";
+        listItemHeader.fontSize = 18;
+    });
+
+    return listItemHeader;
+};
+
+const listItemDetails = (obj, listItemStack) => {
+    var listItemStackDetails = new GUI.TextBlock();
+    listItemStackDetails.width = "100%";
+    listItemStackDetails.height = "40px";
+    listItemStackDetails.marginBottom = "10px";
+
+    listItemStackDetails.text = `Details: Length ${obj.length}, Width ${obj.width}`;
+    listItemStackDetails.color = "white"; 
+
+    listItemStack.addControl(listItemStackDetails);
+
+    return listItemStackDetails;
+};
+
+
+
+
 
