d7891652af5a4035250fd0dcac60e7f1d3375efd
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-03-07 09:46:04 -0800

    add local storage
    author: dangkyle64
    date: 3/7/2025

--- app/render/_components/FurnitureMenu/FurnitureMenu.js
+++ app/render/_components/FurnitureMenu/FurnitureMenu.js
@@ -3,7 +3,11 @@ import { FurnitureMenuButton } from'./FurnitureMenuButton';
 import { fetchFurnitureData } from '../../../_furnitureApi/furnitureApi';
 export const FurnitureMenu = async (scene) => {
     
-    const furnitureData = await fetchFurnitureData();
+    let furnitureData = loadLocalFurnitureData();
+    if (!furnitureData) {
+        furnitureData = await fetchFurnitureData();
+        saveLocalFurnitureData(furnitureData);
+    };
 
     var interactionMenuVisible = false; 
     var interactionMenu = InteractionMenu(scene, furnitureData);
@@ -19,3 +23,12 @@ export const FurnitureMenu = async (scene) => {
 
     FurnitureMenuButton(scene, toggleInteractionMenu);
 };
+
+const saveLocalFurnitureData = (data) => {
+    localStorage.setItem('furnitureData', JSON.stringify(data));
+};
+
+const loadLocalFurnitureData = () => {
+    const data = localStorage.getItem('furnitureData');
+    return data ? JSON.parse(data) : null;
+}
